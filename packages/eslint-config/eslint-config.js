14880848322611536d4a85db07d11a67e12b20f5
Kristopher Cargile <kris@kriscargile.com>
2022-10-07 21:52:04 -0600

    prune old cfg file

--- packages/eslint-config/eslint-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	root: true,
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended"],
-	ignorePatterns: [".turbo", "coverage", "dist", "test", ".eslintrc.js"],
-	plugins: ["@typescript-eslint"]
-};
